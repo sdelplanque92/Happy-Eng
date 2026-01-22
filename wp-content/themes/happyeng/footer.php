6e09b2d71dcf6462673840b296e7630781d4a5b2
Sebastien <sdelplanque92@gmail.com>
2026-01-22 22:37:51 +0100

    main: first commit

---
+++ wp-content/themes/happyeng/footer.php
@@ -0,0 +1,87 @@
+<footer id="footer" class="footer-section">
+    <div class="container">
+        <div class="footer-content pt-5 pb-5">
+            <div class="row">
+                <div class="col-xl-4 col-lg-4 mb-50">
+                    <div class="footer-widget">
+                        <div class="footer-logo">
+                            <a href="index.html" class="logo d-flex align-items-center">
+                                <h1>Grayson<span>.</span></h1>
+                            </a>
+                        </div>
+                        <div class="footer-text">
+                            <p>Lorem ipsum dolor sit amet, consec tetur adipisicing elit, sed do eiusmod tempor incididuntut consec tetur adipisicing
+                                elit,Lorem ipsum dolor sit amet.</p>
+                        </div>
+                        <div class="footer-social-icon">
+                            <span>Follow us</span>
+                            <a href="#"><i class="fa fa-facebook facebook-bg"></i></a>
+                            <a href="#"><i class="fa fa-twitter twitter-bg"></i></a>
+                            <a href="#"><i class="fa fa-google google-bg"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-6 mb-50">
+                    <div class="footer-widget">
+                        <div class="footer-widget-heading">
+                            <h3>Useful Links</h3>
+                        </div>
+                        <ul>
+                            <li><a href="index.html">Home</a></li>
+                            <li><a href="about.html">About</a></li>
+                            <li><a href="services.html">Services</a></li>
+                            <li><a href="testimonials.html">Testimonials</a></li>
+                            <li><a href="contact.html">Contact</a></li>
+                            <li><a href="about.html">About us</a></li>
+                            <li><a href="blogs.html">Our News</a></li>
+                            <li><a href="services.html">Our Services</a></li>
+                            <li><a href="team.html">Our Team</a></li>
+                            <li><a href="packages.html">Packages</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-6 mb-50">
+                    <div class="footer-widget">
+                        <div class="footer-widget-heading">
+                            <h3>Newsletter</h3>
+                        </div>
+                        <div class="footer-text mb-25">
+                            <p>Don't miss to subscribe to our new feeds, kindly fill the form below.</p>
+                        </div>
+                        <div class="subscribe-form">
+                            <form action="#">
+                                <input type="text" placeholder="Email Address">
+                                <button><i class="fa fa-telegram"></i></button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="copyright-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-6 col-lg-6 text-left text-lg-left">
+                    <div class="copyright-text">
+                        <p>Grayson<span>.</span> © 2022 - Designed by Zr Themes.</p>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6 d-none d-lg-block">
+                    <div class="footer-menu text-right">
+                        <ul>
+                            <li><a href="index.html">Home</a></li>
+                            <li><a href="terms.html">Terms</a></li>
+                            <li><a href="policy.html">Privacy</a></li>
+                            <li><a href="policy.html">Policy</a></li>
+                            <li><a href="contact.html">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ wp-content/themes/happyeng/functions.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Theme functions.
+ */
+
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+
+add_action( 'after_setup_theme', function () {
+    add_theme_support( 'title-tag' );
+    add_theme_support( 'post-thumbnails' );
+    add_theme_support( 'html5', [ 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption', 'style', 'script' ] );
+
+    register_nav_menus( [
+        'primary' => __( 'Primary Menu', 'freelancer-tech-it' ),
+        'footer'  => __( 'Footer Menu', 'freelancer-tech-it' ),
+    ] );
+} );
+
+add_action( 'wp_enqueue_scripts', function () {
+    $uri = get_template_directory_uri();
+
+    // Google Fonts (from template)
+    wp_enqueue_style( 'fti-google-fonts-open-sans', 'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@300;400;500;600;700;1,300;1,400;1,500;1,600;1,700&display=swap', [], null );
+    wp_enqueue_style( 'fti-google-fonts-comfortaa', 'https://fonts.googleapis.com/css2?family=Comfortaa:wght@700&display=swap', [], null );
+    wp_enqueue_style( 'fti-google-fonts-poppins', 'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap', [], null );
+
+    // Vendor CSS
+    wp_enqueue_style( 'fti-aos',          $uri . '/assets/vendor/aos/aos.css', [], null );
+    wp_enqueue_style( 'fti-glightbox',    $uri . '/assets/vendor/glightbox/css/glightbox.min.css', [], null );
+    wp_enqueue_style( 'fti-swiper',       $uri . '/assets/vendor/swiper/swiper-bundle.min.css', [], null );
+    wp_enqueue_style( 'fti-fontawesome',  $uri . '/assets/stylesheets/font-awesome.min.css', [], null );
+    wp_enqueue_style( 'fti-bootstrapicons',$uri . '/assets/vendor/bootstrap-icons/bootstrap-icons.css', [], null );
+    wp_enqueue_style( 'fti-bootstrap',    $uri . '/assets/vendor/bootstrap/css/bootstrap.min.css', [], null );
+
+    // Theme CSS
+    wp_enqueue_style( 'fti-theme',        $uri . '/assets/stylesheets/styles.css', [ 'fti-bootstrap' ], null );
+
+    // Scripts (keep template order)
+    wp_enqueue_script( 'fti-jquery',      $uri . '/assets/javascripts/jquery.min.js', [], null, true );
+    wp_enqueue_script( 'fti-glightbox',   $uri . '/assets/vendor/glightbox/js/glightbox.min.js', [ 'fti-jquery' ], null, true );
+    wp_enqueue_script( 'fti-bootstrap',   $uri . '/assets/vendor/bootstrap/js/bootstrap.bundle.min.js', [ 'fti-jquery' ], null, true );
+    wp_enqueue_script( 'fti-aos',         $uri . '/assets/vendor/aos/aos.js', [ 'fti-jquery' ], null, true );
+    wp_enqueue_script( 'fti-swiper',      $uri . '/assets/vendor/swiper/swiper-bundle.min.js', [ 'fti-jquery' ], null, true );
+    wp_enqueue_script( 'fti-plugins',     $uri . '/assets/javascripts/plugins.js', [ 'fti-jquery' ], null, true );
+    wp_enqueue_script( 'fti-validator',   $uri . '/assets/javascripts/validator.min.js', [ 'fti-jquery' ], null, true );
+    wp_enqueue_script( 'fti-contactform', $uri . '/assets/javascripts/contactform.js', [ 'fti-jquery' ], null, true );
+    wp_enqueue_script( 'fti-main',        $uri . '/assets/javascripts/main.js', [ 'fti-jquery' ], null, true );
+} );
+
+/**
+ * Helper: render a saved static "main" fragment for pages converted from HTML.
+ */
+function fti_render_static_main( string $template_slug ): void {
+    $file = get_template_directory() . '/template-parts/static/' . $template_slug . '.php';
+    if ( file_exists( $file ) ) {
+        include $file;
+    } else {
+        echo '<main id="main" class="container py-5"><p>Static template part not found: ' . esc_html( $template_slug ) . '</p></main>';
+    }
+}
+
+/**
+ * Register custom menu.
+ */
+function he_custom_menu() {
+    add_theme_support('menus');
+
+    register_nav_menus([
+        'primary' => __('Primary Menu', 'happyeng'),
+        'footer'  => __('Footer Menu', 'happyeng'),
+    ]);
+}
+
+add_action('after_setup_theme', 'he_custom_menu');
